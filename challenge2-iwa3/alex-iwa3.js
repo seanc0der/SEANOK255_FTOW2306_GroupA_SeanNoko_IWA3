505180a7ed2a80b9fa7b10fea9e18c7f92617ab3
seanc0der <sean.noko@gmail.com>
2023-08-03 14:56:54 +0200

    Added challenge 1 & 2 folders, with their respective files and codebases as per the LMS

---
+++ challenge2-iwa3/alex-iwa3.js
@@ -0,0 +1,8 @@
+// alex.js
+
+private firstname = "Alex"
+private surname = "Naidoo"
+public role = "Head of Marketing"
+
+private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
